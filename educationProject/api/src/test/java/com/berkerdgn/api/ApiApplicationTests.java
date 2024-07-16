756d93dce45aacd95c6873cda67606eec450d10f
Berk Erdoğan <erdgnberk@gmail.com>
2024-07-16 18:49:30 +0300

    sample project
    sample project start

---
+++ educationProject/api/src/test/java/com/berkerdgn/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berkerdgn.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
